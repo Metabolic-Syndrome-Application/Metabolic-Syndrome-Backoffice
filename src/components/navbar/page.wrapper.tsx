6f6ebd33fc2e2890b949bef9bca0a0ec75930ee2
NuntNonthicha <69186692+NuntNonthicha@users.noreply.github.com>
2024-03-29 07:42:11 +0700

    chore(del): del all file

--- src/components/navbar/page.wrapper.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react';
-
-export default function PageWrapper({ children }: { children: ReactNode }) {
-  return (
-    <div className='flex flex-grow flex-col space-y-2 bg-[#FAFCFB] px-4 pb-4 pt-2'>
-      {children}
-    </div>
-  );
-}
--- src/constant/config.ts
+++
@@ -1,5 +0,0 @@
-export const siteConfig = {
-  title: 'Metaplan',
-  description: 'Metaplan',
-  url: 'https://tsnext-tw.thcl.dev',
-};
