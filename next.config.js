96f16caa7986c4cdd3b8631902ce3c1a7950a330
NuntNonthicha <69186692+NuntNonthicha@users.noreply.github.com>
2024-04-17 03:32:03 +0700

    chore: test api

--- next.config.js
+++ next.config.js
@@ -11,7 +11,6 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'standalone',
-  backendUrl: process.env.NEXT_PUBLIC_BACKEND_URL,
 
   webpack(config) {
     // Grab the existing rule that handles SVG imports
--- src/config/api.ts
+++ src/config/api.ts
@@ -1,7 +1,6 @@
 //export const BACKEND_URL = 'http://localhost:8000';
-//export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || '';
-const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
-export const BACKEND_URL = `${backendUrl}`;
+//export const BACKEND_URL = 'https:/metaplan-api.zantaclaus.dev';
+export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || '';
 
 export const API_PATH = {
   //Authentication
